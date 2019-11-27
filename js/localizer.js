7b3bf8b073a643eb1985fd7e2606dd4969375741
Enrique José Fernández Rodríguez <ejfdezrdgz@gmail.com>
2019-11-27 14:00:15 +0100

    Redone localization path to work on hosted webs

--- js/localizer.js
+++ js/localizer.js
@@ -1,8 +1,9 @@
 const i18next = window.i18next;
 const Backend = window.i18nextXHRBackend;
+var localePath = "";
 
 export var loc_code = localizationCode();
-var localePath = `../resources/locales/${loc_code}.json`;
+localePath = getLocalePath();
 loadLocalization(localePath);
 
 export function loadLocalization(localePath) {
@@ -12,13 +13,19 @@ export function loadLocalization(localePath) {
             backend: {
                 loadPath: localePath
             }
-        }, function (err, t) {
+        }, function(err, t) {
             updateLocalization();
         });
 }
 
 function getLocale() {
     return window.localStorage.getItem("locale");
+}
+
+function getLocalePath() {
+    var loc = localizationCode();
+    var path = `./resources/locales/${loc}.json`;
+    return path;
 }
 
 function updateLocalization() {
@@ -42,11 +49,11 @@ export function languageSelector() {
     var langFlags = document.getElementsByClassName("langFlag");
     for (let item of langFlags) {
         item.addEventListener("click",
-            function (event) {
+            function(event) {
                 event.preventDefault();
                 var language = item.id.split("/")[0].slice(0, -4);
                 window.localStorage.setItem("locale", language);
-                localePath = `../resources/locales/${language}.json`;
+                localePath = getLocalePath();
                 loadLocalization(localePath);
                 document.getElementById("langSelModal").style.display = "none";
             });
